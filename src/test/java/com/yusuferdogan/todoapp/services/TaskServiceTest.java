f62835f6641539f6589bb404b6778328013eb8aa
yusuf erdogan <yusuffx03@gmail.com>
2021-10-26 23:23:12 +0300

    adding tests

---
+++ src/test/java/com/yusuferdogan/todoapp/services/TaskServiceTest.java
@@ -0,0 +1,92 @@
+package com.yusuferdogan.todoapp.services;
+import com.yusuferdogan.todoapp.domain.Task;
+import com.yusuferdogan.todoapp.domain.User;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest
+public class TaskServiceTest {
+
+    Task task = new Task();
+    User user = new User();
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TaskService taskService;
+
+    @BeforeAll
+    void setUp(){
+        user.setEmail("asdfg@gmail.com");
+        user.setPassword("123456");
+        userService.addUser(user);
+//----------------------------------------------- init user
+        System.out.println("All users");
+        for(User u:userService.getAllUsers()){
+            System.out.println(u);
+        }
+        System.out.println("-------------------");
+
+// ---------------------------------------------- print all users
+        task.setTaskName("TESTING TASK NAME ");
+        task.setTaskDescription("TESTING TASK DESCRIPTION");
+        task.setIsDone(false);
+        task.setUser(user);
+        taskService.addTask(task);
+// ---------------------------------------------- init task
+
+        System.out.println("All tasks");
+        for(Task t: taskService.getAllTasks()){
+            System.out.println(t);
+        }
+        System.out.println("----------------------");
+// ---------------------------------------------- print all tasks
+
+    }
+
+    @AfterAll
+    void dispose(){
+        taskService.removeTask(task.getId());
+        userService.removeUser(user.getId());
+        for(Task t: taskService.getAllTasks()){
+            System.out.println(t);
+        }
+    }
+
+    @Test
+    void testGetAllTasks(){
+        Assertions.assertFalse(taskService.getAllTasks().isEmpty());
+    }
+
+    @Test
+    void testGetAllTasksOfUser(){
+        Assertions.assertFalse(taskService.getAllTasksOfUser(user.getemail(),user.getPassword()).isEmpty());
+    }
+
+    @Test
+    void testGetTaskById(){
+        Assertions.assertNotNull(taskService.getTaskById(task.getId()));
+    }
+
+    @Test
+    void testUpdateTask(){
+        final String updated_task_name = "TEST_UPDATE_TASK_NAME";
+        task.setTaskName(updated_task_name);
+        Assertions.assertEquals(updated_task_name,task.getTaskName());
+    }
+
+    @Test
+    void testRemoveTask() {
+        Task testingTask = task;
+        taskService.addTask(testingTask);
+        assert testingTask != null;
+        taskService.removeTask(testingTask.getId());
+        Assertions.assertNotNull(taskService.getTaskById(testingTask.getId()));
+    }
+
+
+
+}
---
+++ src/test/java/com/yusuferdogan/todoapp/services/UserServiceTest.java
@@ -0,0 +1,68 @@
+package com.yusuferdogan.todoapp.services;
+import com.yusuferdogan.todoapp.domain.User;
+import com.yusuferdogan.todoapp.repositories.UserRepository;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserRepository userRepository ;
+
+    User user = new User();
+
+    @Autowired
+    private UserService userService;
+
+    @BeforeAll
+    void setUp() {
+
+        user.setEmail("asdfg@gmail.com");
+        user.setPassword("123456");
+        userService.addUser(user);
+        System.out.println("All users");
+        for(User u:userService.getAllUsers()){
+            System.out.println(u);
+        }
+        System.out.println("-------------------");
+    }
+
+    @AfterAll
+    void dispose(){
+        userService.removeUser(user.getId());
+        System.out.println("All users");
+        for(User u:userService.getAllUsers()){
+            System.out.println(u);
+        }
+    }
+
+    @Test
+    void testFindAll() {
+        Assertions.assertFalse(userService.getAllUsers().isEmpty());
+    }
+
+    @Test
+    void testIsUserExist() {
+        Assertions.assertNotNull(userService.findById(user.getId()));
+    }
+
+    @Test
+    void testIsUserExistById() {
+        Assertions.assertTrue(userService.isUserExist(user.getId()));
+    }
+
+    @Test
+    void testRemoveUser() {
+        User testingUser = user;
+        userService.addUser(testingUser);
+        userService.removeUser(testingUser.getId());
+        Assertions.assertFalse(userService.isUserExist(testingUser.getId()));
+    }
+}
